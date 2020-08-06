3195ca2d7385be715cbdc3d10861938af1a76db1
Robert Oikle <6431161+roikle@users.noreply.github.com>
2020-08-06 19:53:21 -0400

    Code cleanup and documentation

--- src/ui/board.js
+++ src/ui/board.js
@@ -1,39 +1,51 @@
-const Grid = require('../grid/grid');
 const d3 = require('d3');
+const Grid = require('../grid/grid');
 
 // The board class contains the logic for generating the svg game board using d3.
 class Board {
 	constructor(containerId) {
 		this.containerId = containerId;
 		this.container = document.getElementById(this.containerId);
-		//this.width = this.container.innerWidth;
-		//this.height = this.container.innerHeight;
+		// this.width = this.container.innerWidth;
+		// this.height = this.container.innerHeight;
 		this.width = 500;
 		this.height = 500;
 		this.cellSize = 10;
 
-		this.grid = new Grid(this.width/this.cellSize, this.height/this.cellSize);
+		// Create a new grid object and populate it with dead cell values
+		this.grid = new Grid(this.width / this.cellSize, this.height / this.cellSize);
 		this.grid.populateMatrix();
 
-		this.dataset = this.prepareDataset(this.grid.matrix);
+		// Prepare a dataset using the grid values
+		this.dataset = this.prepareDataset(this.grid.getMatrix());
+
 		// Create Board
 		this.createBoard();
 	}
 
-	prepareDataset(dataset) {
+	/**
+	 * Converts gridValues into an array of objects which can be handled more
+	 * easily by the d3 visualization.
+	 * @param {array} gridValues A multi-dimensional array of 0s or 1s which
+	 * represent dead and alive cells.
+	 * @return {array} The returned dataset contains a list of objects with
+	 * the x, y, and cell state defined.
+	 */
+	prepareDataset(gridValues) {
 		let i;
 		let j;
 		let colour;
-		const data = [];
+		const dataset = [];
 		const that = this;
-		for (i = 0; i < dataset.length; i += 1) {
-			for (j = 0; j < dataset[i].length; j += 1) {
+
+		for (i = 0; i < gridValues.length; i += 1) {
+			for (j = 0; j < gridValues[i].length; j += 1) {
 				colour = '#ffffff';
-				if (dataset[i][j] === 1) {
+				if (gridValues[i][j] === 1) {
 					colour = '#000000';
 				}
 
-				data.push({
+				dataset.push({
 					x: i * that.cellSize,
 					y: j * that.cellSize,
 					fill: colour,
@@ -41,26 +53,28 @@ class Board {
 			}
 		}
 
-		return data;
+		return dataset;
 	}
 
+	// Add the game board to the user interface, which consists of a svg with
+	// a collection of rectangles representing cells.
 	createBoard() {
-		// clear container
+		// Clear container
 		this.container.innerText = '';
 
 		// Add SVG to container
-		let svg = d3.select('#' + this.containerId)
-			.append("svg")
-			.attr("width", this.width + 'px')
-			.attr("height", this.height + 'px');
+		const svg = d3.select(`#${this.containerId}`)
+			.append('svg')
+			.attr('width', `${this.width}px`)
+			.attr('height', `${this.height}px`);
 
 		// Generate cell rectangles
 		svg.selectAll('rect')
 			.data(this.dataset)
 			.join('rect')
-			.attr('x', d => d.x)
-			.attr('y', d => d.y)
-			.attr('fill', d => d.fill)
+			.attr('x', (d) => d.x)
+			.attr('y', (d) => d.y)
+			.attr('fill', (d) => d.fill)
 			.attr('height', this.cellSize)
 			.attr('width', this.cellSize);
 	}
